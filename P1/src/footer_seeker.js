d46dc550881fd1ea697cce24ab1a44d854c6980a
amy-alstj <113132546+amy-alstj@users.noreply.github.com>
2023-10-10 11:33:04 -0400

    minor change of replacing to script

--- P1/src/footer_seeker.js
+++ P1/src/footer_seeker.js
@@ -4,7 +4,7 @@ document.write(`
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="./main_after_login.html" class="flex items-center mb-4 sm:mb-0">
                     <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" /> -->
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap">Pet Pal</span>
+                    <span class="self-center text-gray-600 text-2xl font-semibold whitespace-nowrap">Pet Pal</span>
                 </a>
                 <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
                     <li>
