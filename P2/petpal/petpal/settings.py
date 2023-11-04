ffe8739d80a0373a2a8ca7eef1e0b7c11af1d233
amy-alstj <113132546+amy-alstj@users.noreply.github.com>
2023-11-04 18:11:00 -0400

    added accounts to settings

--- P2/petpal/petpal/settings.py
+++ P2/petpal/petpal/settings.py
@@ -27,17 +27,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.PetSeeker'  # for custom model 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts",
+    "phonenumber_field",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "pet_listing",
+    "pet_listing", 
+    
 ]
 
 MIDDLEWARE = [
