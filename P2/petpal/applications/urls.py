3317bcafb640e55287fc510dda6b08b152b87937
Jiwoo Jung <jiwoo0206.jj@gmail.com>
2023-11-04 18:06:54 -0400

    removed appliations

--- P2/petpal/applications/urls.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.urls import path
-from .views import PetListingCreateView
-
-app_name = "applications"
-urlpatterns = [path("", PetListingCreateView.as_view(), name="pet_listing")]
