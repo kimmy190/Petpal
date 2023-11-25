73cfdf96fc7504d11c81aa588e18805e22512fab
Fire100 <ilancito70@gmail.com>
2023-11-25 13:40:35 -0500

    Tailwind

--- P3/frontend/petpal/src/pages/PetListing/index.jsx
+++ P3/frontend/petpal/src/pages/PetListing/index.jsx
@@ -1,8 +1,10 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const PetListing = (props) => {
   const { pet_listing_id } = useParams();
+  const navigate = useNavigate();
+
   const [petData, setPetData] = useState();
   useEffect(() => {
     fetch(`/pet_listing/${pet_listing_id}`, {
@@ -12,15 +14,24 @@ const PetListing = (props) => {
         accept: "application/json",
       },
     })
-      .then((response) => response.text())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+      })
       .then((json) => {
-        console.log(json);
         setPetData(json);
       })
-      .catch((error) => console.log("error", error));
-  }, [pet_listing_id]);
+      .catch((error) => navigate("/home"));
+  }, [pet_listing_id, navigate]);
 
-  return <>{petData}</>;
+  return (
+    <div className="flex flex-col justify-center items-center bg-gray-50 py-3">
+      Test
+    </div>
+  );
 };
 
 export default PetListing;
