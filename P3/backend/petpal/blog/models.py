cfc9d4e30cc798a0017575752734d9655aa0743e
Fire100 <ilancito70@gmail.com>
2023-12-03 22:34:02 -0500

    Fix blog model

--- P3/backend/petpal/blog/models.py
+++ P3/backend/petpal/blog/models.py
@@ -7,4 +7,4 @@ class BlogPost(models.Model):
         PetSeeker, on_delete=models.CASCADE, related_name="blogs"
     )
     message = models.TextField()
-    image = models.ImageField(upload_to="logo_images/", null=True, blank=True)
+    image = models.ImageField(upload_to="blog_images/", null=True, blank=True)
