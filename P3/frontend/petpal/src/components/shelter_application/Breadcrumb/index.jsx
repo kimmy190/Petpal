2fc989919bab69a6cbd6c1f58bf5afa71dd8c7fc
Jiwoo Jung <jiwoo0206.jj@gmail.com>
2023-12-08 00:24:13 -0500

    chatbox

--- P3/frontend/petpal/src/components/shelter_application/Breadcrumb/index.jsx
+++ P3/frontend/petpal/src/components/shelter_application/Breadcrumb/index.jsx
@@ -23,7 +23,7 @@ const Breadcrumb = () => {
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="m1 9 4-4-4-4" />
                         </svg>
-                        <Link to="/applications" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
+                        <Link to="/applications/" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
                             List of Applications
                         </Link>
                     </div>
@@ -35,9 +35,9 @@ const Breadcrumb = () => {
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="m1 9 4-4-4-4" />
                         </svg>
-                        <Link to="/current_application" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
+                        <div className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
                             Current Application
-                        </Link>
+                        </div>
                     </div>
                 </li>
             </ol>
--- P3/frontend/petpal/src/components/shelter_application/Chatbox/index.jsx
+++ P3/frontend/petpal/src/components/shelter_application/Chatbox/index.jsx
@@ -1,16 +1,121 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../../contexts/UserContext";
 
-
-const Chatbox = () => {
+const Chatbox = ({ }) => {
     const [isChatboxOpen, setIsChatboxOpen] = useState(false);
+    const [chatHistory, setChatHistory] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+    const { application_id } = useParams();
+    const { user, token } = useContext(UserContext);
+    const [shelterName, setShelterName] = useState();
+    const [userName, setUserName] = useState();
 
     const handleChatboxToggle = () => {
         setIsChatboxOpen(!isChatboxOpen);
     };
 
+    const handleInputChange = (e) => {
+        setNewMessage(e.target.value);
+    };
+
+    const handleSend = async () => {
+
+        const response = await fetch(`http://localhost:8000/comments/application/${application_id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`, // Include the token in the Authorization header
+            },
+            body: JSON.stringify({ body: newMessage }),
+        });
+        if (response.ok) {
+            // Fetch the updated chat history after sending the message
+            fetchChatHistory();
+            // Clear the input field
+            setNewMessage('');
+        } else {
+            console.error('Error sending message:', response.statusText);
+            // Handle error as needed
+        }
+    };
+
+    const fetchChatHistory = async () => {
+
+        const response = await fetch(`http://localhost:8000/comments/application/${application_id}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            setChatHistory(data.results);
+        } else {
+            console.error('Error fetching chat history:', response.statusText);
+            // Handle error as needed
+        }
+    };
+
+    const fetchShelterName = async () => {
+        try {
+            const response = await fetch(`/applications/${application_id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setShelterName(data.shelter.organization_name);
+            } else {
+                console.error('Error fetching shelter name:', response.statusText);
+                setShelterName('Unknown Shelter');
+            }
+        } catch (error) {
+            console.error('Error fetching shelter name:', error);
+            setShelterName('Unknown Shelter');
+        }
+    };
+
+    const fetchUserName = async () => {
+        try {
+            const response = await fetch(`/applications/${application_id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setUserName(data.applicant.username);
+            } else {
+                console.error('Error fetching user name:', response.statusText);
+                setUserName('Unknown User');
+            }
+        } catch (error) {
+            console.error('Error fetching user name:', error);
+            setUserName('Unknown User');
+        }
+    };
+
+
+    useEffect(() => {
+        if (isChatboxOpen) {
+            // Fetch chat history when the chatbox is opened
+            fetchChatHistory();
+        }
+        fetchShelterName();
+        fetchUserName();
+
+    }, [isChatboxOpen]);
+
+
     return (
         <div>
             <div className="grid-cols-1 sm:grid md:grid-cols-1 p-4">
+
+
                 <div className="flex justify-center">
                     <button
                         type="button"
@@ -24,26 +129,126 @@ const Chatbox = () => {
                     </button>
                 </div>
 
+
+
+
                 <section id="chatbox" className={isChatboxOpen ? '' : 'hidden'}>
-                    <div className="container">
-                        <div className="bg-gray-200 rounded-lg">
-                            <div className="bg-blue-500 text-white p-4 flex justify-center mb-4 items-center" style={{ borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }}>
-                                <div className="rounded-full w-[50px] h-[50px] mr-1 mb-1">
-                                    <img src="resources/shelter/shelter_logo.png" alt="Logo"></img>
+                    {/* Header displaying the name of the other communicator */}
+                    <div className="bg-blue-500 text-white p-4 rounded-t-lg">
+                        <div className="flex justify-center">
+                            <p className="text-lg font-semibold">
+                                {/* user */}
+                                {!user.shelter && (
+                                    shelterName
+                                )}
+
+                                {user.shelter && (
+                                    userName
+                                )}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="">
+                        <div
+                            className="bg-gray-200 rounded-b-lg"
+                            style={{ display: 'flex', flexDirection: 'column-reverse', overflowY: 'scroll', maxHeight: '400px', width: '100%' }}
+                        >
+
+                            {/* Chat history */}
+                            {!user.shelter && (
+                                <div className="p-4">
+                                    {chatHistory.slice().reverse().map((message) => (
+                                        <div
+                                            key={message.id}
+                                            className={`message ${message.author === user.id ? 'sent' : 'received'}`}
+                                            style={{
+                                                backgroundColor: message.author === user.id ? '#a7e6a1' : '#ADD8E6',
+                                                padding: '10px',
+                                                margin: '5px',
+                                                borderRadius: '12px',
+                                                alignSelf: 'flex-start',
+                                                width: 'fit-content',
+                                                marginLeft: message.author === user.id ? 'auto' : '',
+                                            }}
+                                        >
+                                            <p
+                                                style={{
+                                                    fontSize: '0.9rem',
+                                                    color: message.author === user.id ? '#333' : '#666',
+                                                    textAlign: message.author === user.id ? 'right' : 'left',
+                                                }}
+                                            >
+                                                {message.body}
+                                            </p>
+                                            <p
+                                                style={{
+                                                    fontSize: '0.8rem',
+                                                    color: '#888',
+                                                    textAlign: message.author === user.id ? 'right' : 'left',
+                                                    marginTop: '5px',
+                                                }}
+                                            >
+                                                {message.author === user.id ? 'You' : `${shelterName}`}
+                                            </p>
+                                        </div>
+                                    ))}
                                 </div>
-                                <div className="inline-block rounded px-6 pb-[6px] pt-2 font-bold" style={{ height: 'fit-content' }} data-te-ripple-init id="liveChatButton">
-                                    Toronto Adoption Shelter
+                            )}
+
+                            {user.shelter && (
+                                <div className="p-4">
+                                    {chatHistory.slice().reverse().map((message) => (
+                                        <div
+                                            key={message.id}
+                                            className={`message ${message.author === user.shelter.id ? 'sent' : 'received'}`}
+                                            style={{
+                                                backgroundColor: message.author === user.shelter.id ? '#a7e6a1' : '#ADD8E6',
+                                                padding: '10px',
+                                                margin: '5px',
+                                                borderRadius: '12px',
+                                                alignSelf: 'flex-start',
+                                                width: 'fit-content',
+                                                marginLeft: message.author === user.shelter.id ? 'auto' : '',
+                                            }}
+                                        >
+                                            <p
+                                                style={{
+                                                    fontSize: '0.9rem',
+                                                    color: message.author === user.shelter.id ? '#333' : '#666',
+                                                    textAlign: message.author === user.shelter.id ? 'right' : 'left',
+                                                }}
+                                            >
+                                                {message.body}
+                                            </p>
+                                            <p
+                                                style={{
+                                                    fontSize: '0.8rem',
+                                                    color: '#888',
+                                                    textAlign: message.author === user.shelter.id ? 'right' : 'left',
+                                                    marginTop: '5px',
+                                                }}
+                                            >
+                                                {message.author === user.shelter.id ? 'You' : `${userName}`}
+                                            </p>
+                                        </div>
+                                    ))}
                                 </div>
-                            </div>
-                            <div className="grid-cols-1 sm:grid md:grid-cols-1 p-4">
-                                <div className="mt-4 flex">
-                                    <input
-                                        type="text"
-                                        className="w-3/4 p-2 rounded-l-lg border border-gray-300 focus:outline-none focus:border-blue-500"
-                                        placeholder="Type your message here"
-                                    />
-                                    <button className="w-1/4 bg-blue-500 text-white p-2 rounded-r-lg">Send</button>
-                                </div>
+                            )}
+
+                        </div>
+                        {/* Input for new message */}
+                        <div className="grid-cols-1 sm:grid md:grid-cols-1">
+                            <div className="mt-4 flex">
+                                <input
+                                    type="text"
+                                    value={newMessage}
+                                    onChange={handleInputChange}
+                                    className="w-3/4 p-2 rounded-l-lg border border-gray-300 focus:outline-none focus:border-blue-500"
+                                    placeholder="Type your message here"
+                                />
+                                <button className="w-1/4 bg-blue-500 text-white p-2 rounded-r-lg" onClick={handleSend}>
+                                    Send
+                                </button>
                             </div>
                         </div>
                     </div>
