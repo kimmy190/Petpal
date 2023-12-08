7cd4c0f5a3081aac3424b87bce72bf5faab030c8
Fire100 <ilancito70@gmail.com>
2023-12-08 17:44:26 -0500

    Add species to filter backend

--- P3/backend/petpal/pet_listing/views.py
+++ P3/backend/petpal/pet_listing/views.py
@@ -87,7 +87,15 @@ class PetListingCreateView(PermissionPolicyMixin, ListCreateAPIView):
 
         filter = {}
         filter["status"] = self.request.GET.get("status", "Available")
-        for filter_param in ["shelter", "breed", "age__gte", "age__lte", "size", "gender"]:
+        for filter_param in [
+            "shelter",
+            "breed",
+            "age__gte",
+            "age__lte",
+            "size",
+            "gender",
+            "species",
+        ]:
             if filter_param in self.request.GET:
                 filter[filter_param] = self.request.GET[filter_param]
 
